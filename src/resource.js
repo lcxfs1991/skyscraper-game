b446603bfe2a0faf6bcdb2caa88ec0f12f432f96
LeeHey <lcxfs1991@gmail.com>
2014-09-28 11:25:59 +0800

    v2
    v2

--- src/resource.js
+++ src/resource.js
@@ -1,19 +1,37 @@
 var res = {
-    HelloWorld_png : "res/HelloWorld.png",
-    CloseNormal_png : "res/CloseNormal.png",
-    CloseSelected_png : "res/CloseSelected.png",
     Life_png : "res/life.png",
+    Start_png: "res/start.png",
     FOLLOW_png: "res/follow.png",
     RESTART_png: "res/restart.png",
-    SHARE_png: "res/share.png"
+    SHARE_png: "res/share.png",
+    BRICK_png: "res/brick.png",
+    BRICK_png1: "res/brick.png1",
+    BRICK_png2: "res/brick.png2",
+    BRICK_png3: "res/brick.png3",
+    BRICK_png4: "res/brick.png4",
+    BRICK_png5: "res/brick.png5",
+    BG_png: "res/bg.jpg",
+    GameIntro: "res/gameintro.jpg",
+    GameOver: "res/gameover.jpg"
+
 };
 
 var g_resources = [
     //image
-    res.HelloWorld_png,
-    res.CloseNormal_png,
-    res.CloseSelected_png
-
+    res.Life_png,
+    res.FOLLOW_png,
+    res.Start_png,
+    res.RESTART_png,
+    res.SHARE_png,
+    res.BRICK_png,
+    res.BRICK_png1,
+    res.BRICK_png2,
+    res.BRICK_png3,
+    res.BRICK_png4,
+    res.BRICK_png5,
+    res.BG_png,
+    res.GameIntro,
+    res.GameOver
     //plist
 
     //fnt
